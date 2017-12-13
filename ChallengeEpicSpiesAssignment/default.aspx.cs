b33a58ab7a4aedfa8b8841bc6aabad8a2f4c8e69
George Ralls <rallsiii@yahoo.com>
2017-12-13 15:53:03 -0800

    Created var for min assignment length so as to remove need to change logic- can just change var. Also commented code.

--- ChallengeEpicSpiesAssignment/default.aspx.cs
+++ ChallengeEpicSpiesAssignment/default.aspx.cs
@@ -4,13 +4,21 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+//C# app created through DevU as part of Tech Academy curriculum
+//App used to calculate $ of assinging spies to spy assignments for fictitious agency
+//Full biz rules are in project files. Gist is:
+//- get Spy and Assignment name
+//- start date must be 14 days after end of last assignment (else error msg)
+//- cost is $500/day
+//- $1000 "budget fee" if assignment > 21 days
 
 namespace ChallengeEpicSpiesAssignment
 {
     public partial class _default : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
-        {if (!Page.IsPostBack)
+        {
+            if (!Page.IsPostBack)
             {
                 previousCalendar.SelectedDate = DateTime.Now.Date;
                 startCalendar.SelectedDate = DateTime.Now.Date.AddDays(14);
@@ -21,39 +29,43 @@ namespace ChallengeEpicSpiesAssignment
 
         protected void AssignButton_Click(object sender, EventArgs e)
         {
-            int breakLength = (int)startCalendar.SelectedDate.Subtract(previousCalendar.SelectedDate).TotalDays;
+            int daysBetweenAssignments = (int)startCalendar.SelectedDate.Subtract(previousCalendar.SelectedDate).TotalDays;
             int assignmentLength = (int)endCalendar.SelectedDate.Subtract(startCalendar.SelectedDate).TotalDays;
             int assignmentMaxDays = 21;
+            int minDaysBetweenAssignments = 14;
             int agentDalyCost = 500;
-            int budgetFee = 1000;
+            int overMaxDaysFee = 1000;
             string agentName = spyCodeNameTextBox.Text;
             string assignmentName = assignmentNameTextBox.Text;
 
-            //Verify period between end of last assignment and start of new one is < 14 days
-            if ( breakLength < 14)
+            //Verify period between end of last assignment and start of new one
+            //(aka breaklenght) is < 14 days. If so, stop and tell user
+            if (daysBetweenAssignments < minDaysBetweenAssignments)
             {
-                resultLabel.Text =
-                    "Union rules require a 14 day break between assignments. Current break is "
-                    + breakLength + " days.<br>Please choose a later start date.";
+                resultLabel.Text = String.Format(
+                    "Union rules require a 14 day break between assignments. Current break is {0} days." +
+                    "<p>Please choose a later start date.", daysBetweenAssignments
+                );
             } //See if assignment is longer than 21 days. If so, calc cost and add $1000
             else if (assignmentLength > assignmentMaxDays)
             {
                 resultLabel.Text = String.Format(
-                    "Assignment of agent {0} to assignement '{1}' is authorized.<br>" +
+                    "Assignment of agent {0} to assignement '{1}' is authorized.<p>" +
                     "Total cost is {2:C} ({3} days * {4:C} per day = {6:C}, + {5:C} Budget Fee = {2:C})",
                     spyCodeNameTextBox.Text,
                     assignmentNameTextBox.Text,
-                    (assignmentLength * agentDalyCost) + budgetFee,
+                    (assignmentLength * agentDalyCost) + overMaxDaysFee,
                     assignmentLength,
                     agentDalyCost,
-                    budgetFee,
+                    overMaxDaysFee,
                     (assignmentLength * agentDalyCost)
                     );
-            } //Calculate cost
+            } //We're here so break must be >= 14 days and assignment <= 21 day
+            //let's calculate the cost!
             else
             {
                 resultLabel.Text = String.Format(
-                    "Assignment of agent {0} to assignement '{1}' is authorized.<br>" +
+                    "Assignment of agent {0} to assignement '{1}' is authorized.<p>" +
                     "Total cost is {2:C} ({3} days * {4:C} per day = {2:C}).",
                     spyCodeNameTextBox.Text,
                     assignmentNameTextBox.Text,
