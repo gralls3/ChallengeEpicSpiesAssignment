71ae554d9732b925a9f071a922078becc625123f
George Ralls <rallsiii@yahoo.com>
2017-12-13 15:11:19 -0800

    Added var for Max # days.

---
+++ ChallengeEpicSpiesAssignment/default.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="ChallengeEpicSpiesAssignment._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <p>
+                <asp:Image ID="spyImage" runat="server" Height="190px" ImageUrl="~/Images/epic-spies-logo.jpg" />
+                <br />
+            </p>
+            <h2>Spy New Assignment Form</h2>
+            <p>
+                <asp:Label ID="codeNameLabel" runat="server" Text="Spy Code Name:"></asp:Label>
+&nbsp;<asp:TextBox ID="spyCodeNameTextBox" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                <asp:Label ID="assignmentNameLabel" runat="server" Text="New Assignment Name:"></asp:Label>
+&nbsp;
+                <asp:TextBox ID="assignmentNameTextBox" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                <asp:Label ID="prevAssignCalLabel" runat="server" Text="End Date of Previous Assignment"></asp:Label>
+                <asp:Calendar ID="previousCalendar" runat="server"></asp:Calendar>
+            </p>
+            <p>
+                <asp:Label ID="newAssignCalLabel" runat="server" Text="Start Date of New Assignment"></asp:Label>
+                <asp:Calendar ID="startCalendar" runat="server"></asp:Calendar>
+            </p>
+            <p>
+                <asp:Label ID="endAssignCalLabel" runat="server" Text="Projected End Date of New Assignment"></asp:Label>
+                <asp:Calendar ID="endCalendar" runat="server"></asp:Calendar>
+            </p>
+            <p>
+                <asp:Button ID="AssignButton" runat="server" Text="Assign Spy" OnClick="AssignButton_Click" />
+            </p>
+            <p>
+                <asp:Label ID="resultLabel" runat="server"></asp:Label>
+            </p>
+            <p>
+                &nbsp;</p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeEpicSpiesAssignment/default.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssignment
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {if (!Page.IsPostBack)
+            {
+                previousCalendar.SelectedDate = DateTime.Now.Date;
+                startCalendar.SelectedDate = DateTime.Now.Date.AddDays(14);
+                endCalendar.SelectedDate = DateTime.Now.Date.AddDays(21);
+            }
+
+        }
+
+        protected void AssignButton_Click(object sender, EventArgs e)
+        {
+            int breakLength = (int)startCalendar.SelectedDate.Subtract(previousCalendar.SelectedDate).TotalDays;
+            int assignmentLength = (int)endCalendar.SelectedDate.Subtract(startCalendar.SelectedDate).TotalDays;
+            int assignmentMaxDays = 21;
+            int agentDalyCost = 500;
+            int budgetFee = 1000;
+            string agentName = spyCodeNameTextBox.Text;
+            string assignmentName = assignmentNameTextBox.Text;
+
+            //Verify period between end of last assignment and start of new one is < 14 days
+            if ( breakLength < 14)
+            {
+                resultLabel.Text =
+                    "Union rules require a 14 day break between assignments. Current break is "
+                    + breakLength + " days.<br>Please choose a later start date.";
+            } //See if assignment is longer than 21 days. If so, calc cost and add $1000
+            else if (assignmentLength > assignmentMaxDays)
+            {
+                resultLabel.Text = String.Format(
+                    "Assignment of agent {0} to assignement '{1}' is authorized.<br>" +
+                    "Total cost is {2:C} ({3} days * {4:C} per day = {6:C}, + {5:C} Budget Fee = {2:C})",
+                    spyCodeNameTextBox.Text,
+                    assignmentNameTextBox.Text,
+                    (assignmentLength * agentDalyCost) + budgetFee,
+                    assignmentLength,
+                    agentDalyCost,
+                    budgetFee,
+                    (assignmentLength * agentDalyCost)
+                    );
+            } //Calculate cost
+            else
+            {
+                resultLabel.Text = String.Format(
+                    "Assignment of agent {0} to assignement '{1}' is authorized.<br>" +
+                    "Total cost is {2:C} ({3} days * {4:C} per day = {2:C}).",
+                    spyCodeNameTextBox.Text,
+                    assignmentNameTextBox.Text,
+                    (assignmentLength * agentDalyCost),
+                    assignmentLength,
+                    agentDalyCost
+                );
+            }
+        }
+    }
+}
